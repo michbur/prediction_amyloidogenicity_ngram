e7f99d66a4e2c435970a787faae6653846de9964
michbur <michalburdukiewicz@gmail.com>
2016-06-10 10:45:24 +0200

    first commit

---
+++ functions/publication_functions/pub_functions.R
@@ -0,0 +1,214 @@
+source("./functions/choose_properties.R")
+source("./functions/create_encodings.R")
+source("./functions/encode_amyloids.R")
+source("./functions/cv.R")
+source("./functions/cv_analysis.R")
+source("./functions/make_classifier.R")
+source("./functions/plot_tools.R")
+
+if(Sys.info()["nodename"] == "tobit" )
+  setwd("~/Dropbox/doktorat/moje_prace/amyloid_ngram2/")
+
+library(biogram) #calc_ed
+require(seqinr) #for choose_properties
+require(xtable)
+require(reshape2)
+library(dplyr)
+
+# read data ----------------------------------------
+
+amyloids <- read.csv("./results/all_summaries.csv") %>% 
+  select(-enc, -partition) %>% 
+  arrange(enc_adj, pos, len_range) 
+
+load("./results/aa_groups.RData")
+aa_groups <- string2list(aa_groups)
+aa_groups[1L:2] <- c(aa1 = string2list(gsub("-", "_", tolower("ADEGHKNPQRST-C-FY-ILMV-W"))),
+                     aa2 = string2list(gsub("-", "_", tolower("AG-C-DEKNPQRST-FILMVWY-H"))))
+
+#############this part is copied from function choose_properties ################
+prop_MK <- read.csv2("./data/AA_index_mk2.csv") %>% filter(!is.na(chosen))
+
+years <- prop_MK %>% select(name) %>% unlist %>% as.character %>% sapply(function(i) 
+  strsplit(last(strsplit(i, ", ")[[1]]), ")", fixed = TRUE)[[1]][1]) %>%
+  as.numeric
+
+prop_MK <- cbind(prop_MK, years = years) %>% filter(years >= 1980) %>%
+  select(X, name) %>% rbind(., 
+                            data.frame(X = 545:550, 
+                                       name =  c("Values of Wc in proteins from class Beta, cutoff 6 A, separation 5 (Wozniak-Kotulska, 2014)",
+                                                 "Values of Wc in proteins from class Beta, cutoff 8 A, separation 5 (Wozniak-Kotulska, 2014)",
+                                                 "Values of Wc in proteins from class Beta, cutoff 12 A, separation 5 (Wozniak-Kotulska, 2014)",
+                                                 "Values of Wc in proteins from class Beta, cutoff 6 A, separation 15 (Wozniak-Kotulska, 2014)",
+                                                 "Values of Wc in proteins from class Beta, cutoff 8 A, separation 15 (Wozniak-Kotulska, 2014)",
+                                                 "Values of Wc in proteins from class Beta, cutoff 12 A, separation 15 (Wozniak-Kotulska, 2014)")))
+#################################################################################
+
+trait_tab <- read.csv(file = "./results/trait_tab.csv")
+
+# best ecodings ----------------------------------------------
+
+best_positions <- amyloids %>% 
+  select(len_range, enc_adj, AUC_mean) %>%
+  group_by(len_range) %>%
+  mutate(position = rank(AUC_mean)) %>%
+  ungroup %>%
+  group_by(enc_adj) %>%
+  summarise(cum_rank = sum(position)) %>%
+  arrange(desc(cum_rank)) %>%
+  slice(1)
+
+best_enc <- best_positions[["enc_adj"]]
+
+# amyloids %>% 
+#   filter(enc_adj == best_enc) %>% 
+#   arrange(AUC_mean) %>% 
+#   print(head = FALSE)
+
+load("./results/cv_results_full.RData")
+
+full_alphabet <- lapply(1L:length(cv_results_full[[1]]), function(single_replicate_id) {
+  lapply(1L:5, function(single_fold) 
+    data.frame(cv_results_full[[1]][[single_replicate_id]][[single_fold]][[1]], replicate = floor((single_replicate_id - 1)/15) + 1)
+  )
+}) %>% unlist(recursive = FALSE) %>%
+  do.call(rbind, .) %>% 
+  mutate(MCC = calc_mcc(TP, TN, FP, FN)) %>%
+  select(replicate, len_range, AUC, MCC, Sens, Spec) %>%
+  group_by(replicate, len_range) %>%
+  summarize_each(funs(mean = liberal_mean, sd = liberal_sd), AUC, MCC, Sens, Spec) %>%
+  ungroup %>% 
+  mutate(pos = c(6, 10, 15)[replicate])
+
+load("./results/cv_results_stand.RData")
+
+stand_alphabet <- lapply(cv_results_stand, function(single_enc)
+  lapply(1L:length(single_enc), function(single_replicate_id) {
+    lapply(1L:5, function(single_fold) 
+      data.frame(single_enc[[single_replicate_id]][[single_fold]][[1]], replicate = floor((single_replicate_id - 1)/15) + 1)
+    )
+  }) %>% unlist(recursive = FALSE) %>%
+    do.call(rbind, .) %>% 
+    mutate(MCC = calc_mcc(TP, TN, FP, FN)) %>%
+    select(replicate, len_range, AUC, MCC, Sens, Spec) %>%
+    group_by(replicate, len_range) %>%
+    summarize_each(funs(mean = liberal_mean, sd = liberal_sd), AUC, MCC, Sens, Spec) %>%
+    ungroup %>% 
+    mutate(pos = c(6, 10, 15)[replicate])) %>% 
+  do.call(rbind, .) %>% 
+  mutate(enc_adj = c(rep(1, 12), rep(2, 12))) %>% 
+  select(replicate, len_range, enc_adj, pos, AUC_mean, MCC_mean, 
+         Sens_mean, Spec_mean, AUC_sd, MCC_sd, Sens_sd, Spec_sd) %>% 
+  arrange(enc_adj, pos, len_range) 
+
+amyloids[amyloids[["enc_adj"]] %in% c(1, 2), ] <- stand_alphabet
+
+# properites of benc encoding  --------------------------------------
+
+load("./results/enc_dupes.RData")
+
+best_dupes <- lapply(enc_dupes[["aa_duplicates"]][best_enc], function(single_enc)
+  single_enc %>%
+    sapply(function(i) substr(i, 3, nchar(i))) %>%
+    strsplit("K") %>%
+    lapply(as.numeric) %>%
+    do.call(rbind, .) %>%
+    data.frame %>%
+    select(X1) %>%
+    unlist(use.names = FALSE) - 2)
+
+frequencies <- data.frame(trait_tab[unlist(best_dupes, use.names = FALSE), ] %>%
+                            apply(1, na.omit) %>% unlist %>% table)
+colnames(frequencies) <- c("X", "Freq")
+frequencies$X <- as.numeric(as.character(frequencies$X))
+
+trait_props_all <- prop_MK %>%
+  inner_join(frequencies) %>%
+  arrange(desc(Freq)) %>%
+  select(name, Freq) %>%
+  mutate(prop = Freq/length(unlist(best_dupes, use.names = FALSE))) %>%
+  droplevels() %>%
+  filter(Freq == 11) %>%
+  select(name) %>%
+  unlist() %>%
+  as.character()
+
+traits_best_ids <- filter(frequencies, Freq == 11) %>%
+  select(X) %>%
+  unlist(use.names = FALSE)
+
+best_enc_aa <- do.call(rbind, lapply(1L:length(aa_groups[[best_enc]]), function(i)
+  data.frame(id = i, aa = toupper(aa_groups[[best_enc]][[i]]), stringsAsFactors = FALSE)
+))
+
+norm_props <- normalize_properties()[traits_best_ids, ] %>% 
+  data.frame %>% 
+  cbind(gr = rownames(.), .) %>% 
+  melt(variable = "aa") %>%
+  mutate(aa = as.character(aa))
+  
+best_enc_props <- inner_join(best_enc_aa, norm_props)
+
+# best n-grams
+ngram_freq <- read.csv("./results/ngram_freq.csv")
+
+# distance from the best encoding --------------------------------------
+
+# very time consuming, load from file instead
+# distances = lapply(aa_groups, function(enc) calc_ed(aa_groups[[best_enc]], enc))
+# save(distances, file="results/distances.Rdata")
+load(file = "results/distances.Rdata")
+
+selected_props <- aaprop[frequencies %>% top_n(5) %>% select(X) %>% unlist, ]
+
+props_normalization <- lapply(aa_groups, function(enc) {
+  coords_a <- lapply(aa_groups[[best_enc]], function(single_subgroup) rowMeans(selected_props[, single_subgroup, drop = FALSE]))
+  coords_b <- lapply(enc, function(single_subgroup) rowMeans(selected_props[, single_subgroup, drop = FALSE]))
+  
+  norm_factor <- sum(sapply(coords_a, function(single_coords_a) {
+    distances <- sapply(coords_b, function(single_coords_b) 
+      # vector of distances between groups
+      sqrt(sum((single_coords_a - single_coords_b)^2))
+    )
+    # c(dist = min(distances), id = unname(which.min(distances)))
+    min(distances)
+  }))
+  norm_factor
+})
+
+normalized_distances <- data.frame(enc_adj=seq_along(aa_groups),
+                                   ed=unlist(distances)*unlist(props_normalization))
+
+#for full alphabet
+full_alphabet_groups = lapply(unlist(aa_groups[[1]]), function(x) x)
+coords_a <- lapply(aa_groups[[best_enc]], function(single_subgroup) rowMeans(selected_props[, single_subgroup, drop = FALSE]))
+coords_b <- lapply(full_alphabet_groups, function(single_subgroup) rowMeans(selected_props[, single_subgroup, drop = FALSE]))
+
+norm_factor <- sum(sapply(coords_a, function(single_coords_a) {
+  distances <- sapply(coords_b, function(single_coords_b) 
+    # vector of distances between groups
+    sqrt(sum((single_coords_a - single_coords_b)^2))
+  )
+  # c(dist = min(distances), id = unname(which.min(distances)))
+  min(distances)
+}))
+normalized_distances <- rbind(normalized_distances,c(0, 14*norm_factor)) 
+
+
+ed_dat <- inner_join(amyloids %>% 
+                       mutate(et = factor(ifelse(enc_adj %in% best_enc, "best", ifelse(enc_adj %in% 1L:2, "literature", "")))) %>%
+                       select(len_range, enc_adj, AUC_mean, et) %>%
+                       rbind(select(full_alphabet, AUC_mean, len_range) %>% 
+                               mutate(et = "full alphabet", enc_adj = 0)) %>%
+                       group_by(enc_adj) %>%
+                       summarise(AUC_mean = mean(AUC_mean), et=et[1]), normalized_distances)
+
+ed_dat[["et"]] <- factor(ed_dat[["et"]], labels = c("Encoding", "Best-performing encoding",
+                                                    "Encoding\nfrom literature", "Full alphabet"))
+
+cor.test(~ AUC_mean + ed, ed_dat)
+
+# results of benchmark --------------------------------------
+
+bench_measures <- read.csv("./results/benchmark_allpreds.csv")
+
---
+++ functions/publication_functions/pub_plots.R
@@ -0,0 +1,131 @@
+# plot data ----------------------------------------------
+
+amyloids_plot <- select(amyloids, AUC_mean, MCC_mean, Sens_mean, Spec_mean, pos, len_range, enc_adj) %>%
+  mutate(et = factor(ifelse(enc_adj %in% best_enc, "best", ifelse(enc_adj %in% 1L:2, "literature", "")))) %>%
+  select(AUC_mean, MCC_mean, Sens_mean, Spec_mean, pos, len_range, et, enc_adj) %>%
+  rbind(select(full_alphabet, AUC_mean, MCC_mean, Sens_mean, Spec_mean, pos, len_range) %>% 
+          mutate(et = "full alphabet", enc_adj = 0)) %>%
+  mutate(len_range = factor(len_range, levels = c("[5,6]", "(6,10]", "(10,15]", "(15,25]")),
+         pos = factor(pos, labels = paste0("Training peptide length: ", 
+                                           c("6", "6-10", "6-15"))),
+         et = factor(et, labels = c("Encoding", "Best-performing encoding",
+                                    "Standard encoding", "Full alphabet"))) %>%
+  mutate(len_range = factor(len_range, 
+                            labels = paste0("Test peptide length: ", c("6 ", "7-10", "11-15", "16-25"))))
+
+# Fig 1 all encodings sens/spec  ----------------------------------------
+
+sesp_dat <- amyloids_plot
+levels(sesp_dat[["pos"]]) <- c("Training peptide\nlength: 6", "Training peptide\nlength: 6-10", 
+                               "Training peptide\nlength: 6-15")
+
+sesp_plot <- ggplot(sesp_dat, aes(x = Spec_mean, y = Sens_mean, color = et, shape = et)) +
+  geom_point() +
+  scale_color_manual("", values = c("grey", "red", "blue", "green")) +
+  scale_shape_manual("", values = c(1, 16, 15, 15)) +
+  scale_y_continuous("Mean sensitivity") +
+  scale_x_continuous("Mean specificity") +
+  facet_grid(pos ~ len_range) +
+  my_theme +
+  geom_point(data = filter(sesp_dat, et != "Encoding"), 
+             aes(x = Spec_mean, y = Sens_mean, color = et))
+
+png("./publication/figures/sesp_plot.png", height = 4, width = 6.5, unit = "in", res = 200)
+#cairo_ps("./pub_figures/sesp_plot.eps", height = 4, width = 8)
+# should be eps, but it's too big for overleaf
+print(sesp_plot)
+dev.off()
+
+# Fig 2 AUC boxplot  ----------------------------------------
+
+AUC_boxplot <- ggplot(amyloids_plot, aes(x = len_range, y = AUC_mean)) +
+  geom_boxplot(outlier.color = "grey", outlier.shape = 1) +
+  geom_point(data = filter(amyloids_plot, et != "Encoding"), 
+             aes(x = len_range, y = AUC_mean, color = et, shape = et)) +
+  scale_x_discrete("") +
+  scale_y_continuous("Mean AUC") +
+  scale_shape_manual("", values = c(1, 16, 15, 15), drop = FALSE) +
+  scale_color_manual("", values = c("grey", "red", "blue", "green"), drop = FALSE) +
+  facet_wrap(~ pos, nrow = 3) +
+  my_theme + 
+  coord_flip()
+
+cairo_ps("./publication/figures/AUC_boxplot.eps", height = 3.5, width = 6.5)
+#png("./pub_figures/AUC_boxplot.png", height = 648, width = 648)
+print(AUC_boxplot)
+dev.off()
+
+# Fig 3 MCC boxplot  ----------------------------------------
+
+MCC_boxplot <- ggplot(amyloids_plot, aes(x = len_range, y = MCC_mean)) +
+  geom_boxplot(outlier.color = "grey", outlier.shape = 1) +
+  geom_point(data = filter(amyloids_plot, et != "Encoding"), 
+             aes(x = len_range, y = MCC_mean, color = et, shape = et)) +
+  scale_x_discrete("") +
+  scale_y_continuous("Mean MCC") +
+  scale_shape_manual("", values = c(1, 16, 15, 15), drop = FALSE) +
+  scale_color_manual("", values = c("grey", "red", "blue", "green"), drop = FALSE) +
+  facet_wrap(~ pos, nrow = 3) +
+  my_theme + 
+  coord_flip()
+
+# cairo_ps("./publication/figures/MCC_boxplot.eps", height = 9, width = 8)
+# #png("./pub_figures/MCC_boxplot.png", height = 648, width = 648)
+# MCC_boxplot
+# dev.off()
+
+# Fig 4 properties  ----------------------------------------
+
+ggplot(best_enc_props, aes(x = as.factor(id), y = value, label = aa)) +
+  geom_text(position = "dodge") +
+  facet_wrap(~ gr, ncol = 2)
+
+
+# Fig 5 n-grams  ----------------------------------------
+
+ngram_freq_plot <- mutate(ngram_freq, decoded_name = gsub("_", "|", decoded_name)) %>%
+  mutate(decoded_name = factor(decoded_name, levels = as.character(decoded_name)),
+         amyloid = diff_freq > 0) %>%
+  melt() %>%
+  filter(variable %in% c("pos", "neg")) %>%
+  droplevels %>%
+  mutate(variable = factor(variable, labels = c("Amyloid", "Non-amyloid")))
+
+ngram_plot <- ggplot(ngram_freq_plot, aes(x = decoded_name, y = value)) +
+  geom_bar(aes(fill = variable), position = "dodge", stat = "identity") +
+  geom_point(data = group_by(ngram_freq_plot, decoded_name)  %>% filter(value == max(value)),
+             aes(y = value + 0.002, shape = association)) +
+  scale_fill_manual("", values = c("gold", "darkmagenta")) +
+  scale_shape_manual("Motif:", breaks = c("Amyloidogenic", "Non-amyloidogenic"), values = c(16, 17, NA)) +
+  scale_y_continuous("Frequency") +
+  scale_x_discrete("") +
+  coord_flip() +
+  my_theme +
+  theme(panel.grid.major.y = element_line(color = "lightgrey", size = 0.5)) 
+
+# in case we need to get n-grams in a tabular format
+#writeLines(as.character(ngram_freq_plot[["decoded_name"]]), "n_gramy_Ania.txt")
+
+cairo_ps("./publication/figures/ngrams.eps", height = 8, width = 2.5)
+print(ngram_plot)
+dev.off()
+
+# Fig 6 encoding distance  ----------------------------------------
+
+ed_AUC_plot <- ggplot(ed_dat, aes(x=ed, y=AUC_mean, color=et, shape = et)) + 
+  geom_point() +
+  scale_color_manual("", values = c("grey", "red", "blue", "green")) +
+  scale_shape_manual("", values = c(1, 16, 15, 15), drop = FALSE) +
+  xlab("Normalized encoding distance") +
+  ylab("AUC") +
+  my_theme +
+  geom_point(data = filter(ed_dat, et != "Encoding"), 
+             aes(x = ed, y = AUC_mean, color = et)) +
+  guides(color=guide_legend(ncol=2))
+
+cairo_ps("./publication/figures/ed_AUC.eps", height = 4, width = 3)
+print(ed_AUC_plot)
+dev.off()
+
+# save(amyloids_plot, best_enc_props, ngram_freq_plot, ed_dat,
+#      file = "./presentation/presentation.RData")
